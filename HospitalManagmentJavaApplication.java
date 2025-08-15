a0c3bc4f963031c91868bc83231189e9c4eb333e
sundhar1947 <126653757+sundhar1947@users.noreply.github.com>
2025-08-15 11:15:54 +0530

    Add files via upload

---
+++ HospitalManagmentJavaApplication.java
@@ -0,0 +1,12 @@
+package com.example.Hospitalmanagmentjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagmentJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagmentJavaApplication.class, args);
+    }
+}
